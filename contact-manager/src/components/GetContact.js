dd3d4976527c8839e7f2b4d0a706869db09ffd90
Asif Sayyed <115421661+Asifdotexe@users.noreply.github.com>
2023-09-06 19:44:05 +0530

    v0.4
    - Added Bootstrap to all the pages
    - Added Functionality to add id to update and delete
    to do:
    - Add auto reload after update or delete
    - Fix MockAPI to add phone number automatically

--- contact-manager/src/components/GetContact.js
+++ contact-manager/src/components/GetContact.js
@@ -3,46 +3,43 @@ import axios from 'axios';
 
 function GetContact() {
 
-    const[contactData, setContactData] = useState([])
+    const [contactData, setContactData] = useState([])
 
     useEffect(() => {
         axios.get("https://64f60a932b07270f705e0ad8.mockapi.io/api/contactdotexe/contacts")
-        .then((response) => {
-            console.log(response)
-            setContactData(response.data)
-        })
-    },[])
+            .then((response) => {
+                console.log(response)
+                setContactData(response.data)
+            })
+    }, [])
 
-    return ( 
-        <div>
-        <h1>Welcome to Contact Manager App!</h1>
-        <h2>List of contacts</h2>
-        <table>
-            <tr>
-                <th>ID</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Email ID</th>
-                <th>Phone Number</th>
-            </tr>
-        </table>
-        {contactData.map((data) => {
-            return(
-                <div>
-                    <table>
+    return (
+        <div className="container mt-4">
+            <h2>List of contacts</h2>
+            <table className="table table-bordered">
+                <thead>
                     <tr>
-                        <td>{data.id}</td>
-                        <td>{data.fname}</td>
-                        <td>{data.lname}</td>
-                        <td>{data.email}</td>
-                        <td>{data.pno}</td>
+                        <th>ID</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email ID</th>
+                        <th>Phone Number</th>
                     </tr>
-                    </table>
-                </div>
-            )
-        })}
+                </thead>
+                <tbody>
+                    {contactData.map((data) => (
+                        <tr key={data.id}>
+                            <td>{data.id}</td>
+                            <td>{data.fname}</td>
+                            <td>{data.lname}</td>
+                            <td>{data.email}</td>
+                            <td>{data.pno}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
-     );
+    );
 }
 
 export default GetContact;
--- contact-manager/src/components/PPDContact.js
+++ contact-manager/src/components/PPDContact.js
@@ -1,57 +1,117 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 function PPD() {
-    
-    const data = {fname:'',lname:'',email:'',pno:''}
+  const data = { id: '', fname: '', lname: '', email: '', pno: '' };
+  const [inputData, setInputData] = useState(data);
 
-    const [inputData, setInputData] = useState(data)
+  const handleData = (e) => {
+    setInputData({ ...inputData, [e.target.name]: e.target.value });
+  };
 
-    const handleData= (e) =>[
-        setInputData({...inputData,[e.target.name]: e.target.value})
-    ]
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(
+        "https://64f60a932b07270f705e0ad8.mockapi.io/api/contactdotexe/contacts",
+        inputData
+      )
+      .then((response) => {
+        console.log(response);
+      });
+  };
 
-    const handleSubmit = (e) =>{
-        e.preventDefault();
-        axios.post("https://64f60a932b07270f705e0ad8.mockapi.io/api/contactdotexe/contacts",inputData)
-        .then((response) => {
-            console.log(response)
-        })
-    }
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    axios
+      .put(
+        `https://64f60a932b07270f705e0ad8.mockapi.io/api/contactdotexe/contacts/${inputData.id}`,
+        inputData
+      )
+      .then((response) => {
+        console.log(response);
+      });
+  };
 
-    const handleUpdate = (e) =>{
-        e.preventDefault();
-        axios.put("https://64f60a932b07270f705e0ad8.mockapi.io/api/contactdotexe/contacts/1",inputData)
-        .then((response) => {
-            console.log(response)
-        })
-    }
+  const handleDelete = (e) => {
+    e.preventDefault();
+    axios
+      .delete(
+        `https://64f60a932b07270f705e0ad8.mockapi.io/api/contactdotexe/contacts/${inputData.id}`
+      )
+      .then((response) => {
+        console.log(response);
+      });
+  };
 
-    const handleDelete = (e) =>{
-        e.preventDefault();
-        axios.delete("https://64f60a932b07270f705e0ad8.mockapi.io/api/contactdotexe/11")
-        .then((response) => {
-            console.log(response)
-        })
-    }
-
-    return ( 
-        <div>
-            <form>
-                First Name: 
-                <input type='text' name='fname' value={inputData.fname} onChange={handleData}></input><br></br>
-                Last Name:
-                <input type='text' name='lname' value={inputData.lname} onChange={handleData}></input><br></br>
-                Contact Email:
-                <input type='email' name='email' value={inputData.email} onChange={handleData}></input><br></br>
-                Phone Number:
-                <input type='phonenumber' name='pno' value={inputData.pno} onChange={handleData}></input><br></br>
-                <button onClick={handleSubmit}>Submit</button>
-                <button onClick={handleUpdate}>Update</button>
-                <button onClick={handleDelete}>Delete</button>
-            </form>
+  return (
+    <div className="container mt-4">
+                    <h1>Welcome to Contact Manager App!</h1>
+                    <hr></hr>
+      <form>
+        <div className="mb-3">
+          <label className="form-label">First Name:</label>
+          <input
+            type="text"
+            className="form-control"
+            name="fname"
+            value={inputData.fname}
+            onChange={handleData}
+          />
         </div>
-     );
+        <div className="mb-3">
+          <label className="form-label">Last Name:</label>
+          <input
+            type="text"
+            className="form-control"
+            name="lname"
+            value={inputData.lname}
+            onChange={handleData}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Contact Email:</label>
+          <input
+            type="email"
+            className="form-control"
+            name="email"
+            value={inputData.email}
+            onChange={handleData}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Phone Number:</label>
+          <input
+            type="phonenumber"
+            className="form-control"
+            name="pno"
+            value={inputData.pno}
+            onChange={handleData}
+          />
+        </div>
+        <button className="btn btn-primary" onClick={handleSubmit}>
+          Submit
+        </button>
+        <button className="btn btn-success" onClick={handleUpdate}>
+          Update
+        </button>
+        <button className="btn btn-danger" onClick={handleDelete}>
+          Delete
+        </button><hr></hr>
+        <div className="mb-3">
+          <label className="form-label">ID: (Specify if Update or Delete)</label>
+          <input
+            type="text"
+            className="form-control"
+            name="id"
+            value={inputData.id}
+            onChange={handleData}
+          />
+          <hr></hr>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default PPD;
